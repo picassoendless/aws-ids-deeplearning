85c3003371726a8974a128f5190967619e1900ad
Tendai Nemure <nemuretendai@gmail.com>
2025-05-29 00:17:39 -0400

    Update security groups

--- main.tf
+++ main.tf
@@ -37,7 +37,8 @@ resource "aws_instance" "sensor" {
   ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public.id
-  security_groups        = [aws_security_group.instance_sg.name]
+  //security_groups        = [aws_security_group.instance_sg.name]
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
 
   tags = {
     Name = "ai-ids-sensor"
